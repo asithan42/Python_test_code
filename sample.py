4f12d6c4816b3ac3c59fafe948dfe4bbfbe81e82
asithan42 <asithan42@gmail.com>
2021-09-18 10:35:40 +0530

    Initial commit

---
+++ sample.py
@@ -0,0 +1,31 @@
+print("hello")
+#Example Of Dictionaries
+"""
+x={'name':'sunil','id':6,'age':18}
+print(x)
+x['town']='Nittambuwa'
+print(x)
+del x['id']
+print(x)
+"""
+
+#Example of Arithmatic Oparators
+'''
+y=50.86
+z=10.98
+n=y/z
+print("The value is =",n)
+'''
+#Example of Compurison Oparators
+'''a=18!=19
+print(a)'''
+#Example of Logical Oparators
+'''d=43<=36 and 90>56
+b=43<=43 or 99>111
+m=not(3!=4)
+print(m)'''
+
+#Example Of Bit Wise Oparators
+x=50
+m=x>>2
+print(m)
